5b262d764c049a6b625617f677cfcb00892d0fc0
Gao Sun <gaosun.dev@gmail.com>
2021-07-02 22:41:47 +0800

    feat: add sign-in route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from '@/pages/Home';
-import './scss/normalized.scss';
 import Callback from '@/pages/Callback';
+import SignIn from './pages/SignIn';
+
+import './scss/normalized.scss';
 
 const App = () => (
   <Switch>
     <Route exact path="/" component={Home} />
+    <Route exact path="/sign-in" component={SignIn} />
     <Route exact path="/callback" component={Callback} />
   </Switch>
 );
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SignIn = () => {
+  return <div>Sign-in page</div>;
+};
+
+export default SignIn;
