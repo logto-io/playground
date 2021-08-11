9d316d747786ca7b1669484f0e4fbd3d432521fe
Gao Sun <gaosun.dev@gmail.com>
2021-08-11 20:57:59 +0800

    chore: remove deprecated pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,11 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Callback from './pages/Callback';
-import SignIn from './pages/SignIn';
-import Consent from './pages/Consent';
-
 import './scss/normalized.scss';
 
 const App = () => (
   <Switch>
     <Route exact path="/" component={Home} />
-    <Route exact path="/sign-in" component={SignIn} />
-    <Route exact path="/sign-in/consent" component={Consent} />
     <Route exact path="/callback" component={Callback} />
   </Switch>
 );
